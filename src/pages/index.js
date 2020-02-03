ff7a380b7cb2b71517db0edb061ba4d24f4bfa46
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2020-02-03 15:32:01 +0300

    gatsby app is created

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {FaShoppingCart} from "react-icons/fa"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <FaShoppingCart/>
+  </Layout>
+)
+
+export default IndexPage
