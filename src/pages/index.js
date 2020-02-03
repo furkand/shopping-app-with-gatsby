54f035193e939af2d739e7ace33934145d5d6502
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2020-02-04 01:32:05 +0300

    background image is added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,27 @@
 import React from "react"
+import {Link,graphql} from "gatsby"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {BackgroundPart} from "../components/Globals/BackgroundPart"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <SEO title="Home" keywords={["gatsby",'application', "react" ]}/>
+    <BackgroundPart img={data.img.childImageSharp.fluid} title="Join to the Revolution" styleClass="default-background"/>
 
   </Layout>
 )
 
+export const query = graphql`
+{
+img: file(relativePath: { eq:
+"teslaimg.jpg"}) {
+  childImageSharp{
+    fluid {
+      ...GatsbyImageSharpFluid_tracedSVG 
+      }
+    }
+  }
+}`
 export default IndexPage
