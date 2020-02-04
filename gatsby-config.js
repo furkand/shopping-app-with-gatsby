eb9be2d1c83617f3530ddb2503b804d55525036e
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2020-02-04 15:33:10 +0300

    contentful api is implemented and I decleared env variables in netlify to do not cause any error

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `We Are Tesla`,
@@ -12,6 +15,13 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    },
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
