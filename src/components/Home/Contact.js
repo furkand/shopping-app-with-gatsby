7a31d117ff9d539c06aa92cc1788789c9e641091
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2020-02-04 20:35:26 +0300

    snipcart api is implemented

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -6,7 +6,7 @@ export default function Contact (){
             <Title title="contact us"/>
             <div className="row">
                 <div className="col-10 col-sm-8 col-md-6 mx-auto">
-                    <form >
+                    <form action="https://formspree.io/mnqddejn" method="POST">
                         <div className="form-group">
                             <label htmlFor="name" >Name</label>
                             <input 
