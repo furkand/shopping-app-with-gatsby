e7145935a4bb8a944f3922ca9b6e30c38cc46ed0
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2020-02-04 20:42:37 +0300

    debug

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         apiKey: process.env.SNIPCART_API,
         autopop:true
       }
-    }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/components/Globals/BackgroundPart.js
+++ src/components/Globals/BackgroundPart.js
@@ -2,7 +2,11 @@ import React from "react"
 import BackgroundImage from "gatsby-background-image";
 
 export const BackgroundPart = ({img,styleClass,title,children,imgMobile}) =>{ 
-    const userWidth = this.window.innerWidth
+    const isClient = typeof window !== 'undefined'
+    if(isClient){
+        const userWidth = this.window.innerWidth
+    }
+    
     return (
     <BackgroundImage className={userWidth< 580 ? `${styleClass} mobile-background` : styleClass} fluid={userWidth< 580 ? imgMobile : img} >
         <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold"> 
